46d98456648c6f558b7d2eeb98fe71d87e732e6c
colagratis129 <colagratis129@gmail.com>
2024-07-09 05:54:07 +0700

    add actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://colagratis129.github.io',
+  base: 'jkt-dsgn',
   integrations: [tailwind(), compress(),]
 });
